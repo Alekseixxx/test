96f878a05b19bfbe91f63ac8cbae157858a2387f
Alex <android4443@gmail.com>
2022-07-01 14:27:10 +0200

    first programm

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-{
-  let number = 10;
-}
+const numbersOfFilms = prompt("How much films you see?", "");
 
-console.log(number);
+const personalMovieDB = {
+  count: 0,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+personalMovieDB.count = numbersOfFilms;
+
+const question1 = prompt("Last film you see?");
+const question2 = +prompt("Score last film?");
+const question3 = prompt("Last film you see?");
+const question4 = +prompt("Score last film?");
+
+personalMovieDB.movies[question1] = question2;
+personalMovieDB.movies[question3] = question4;
+
+console.log(personalMovieDB);
